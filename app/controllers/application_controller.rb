6076bcb2d25dd22e184bdade29eacbc070cd7678
hayashi-7 <tomos19088@gmail.com>
2020-09-04 19:21:17 +0900

    ユーザー新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday])
+    user_dates = [:nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday ]
+    devise_parameter_sanitizer.permit :sign_up, keys: user_dates
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday ,presence: true
+         #validates :encrypted_password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+         with_options presence: true do
+          validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+          validates :second_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :second_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :birthday, :nickname
+         end
 end
+ 
