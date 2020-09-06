384100365fbc3022110ade2d6ae3724e6e3ce445
hayashi-7 <tomos19088@gmail.com>
2020-09-06 17:59:47 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_out_path_for(resource)
-    "/"
+  def after_sign_out_path_for(_resource)
+    '/'
    end
+
   protected
 
   def configure_permitted_parameters
-    user_dates = [:nickname, :first_name, :second_name, :first_name_kana, :second_name_kana, :birthday ]
+    user_dates = %i[nickname first_name second_name first_name_kana second_name_kana birthday]
     devise_parameter_sanitizer.permit :sign_up, keys: user_dates
   end
 
@@ -16,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
