6dc77d0459096783ded0c49d66341e948eddc807
hayashi-7 <tomos19088@gmail.com>
2020-09-07 12:23:56 +0900

    モデルコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # validates :encrypted_password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
     validates :second_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
