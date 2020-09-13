e8735fdc30d95a23e69076d29adcf57825240880
hayashi-7 <tomos19088@gmail.com>
2020-09-13 15:59:24 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,34 @@
 class ItemsController < ApplicationController
-  has_one_attached :image
-  def new
-    Item.new(item_params)
+  before_action :set_item, only: %i[edit show]
+
+  def index
+    @items = Item.order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+   end
+  end
+
+  def show; end
+
+  def edit; end
 
   private
 
   def item_params
-    params.require(:item).permit( :image, :text, :price, :name, :category_id, :status_id, :shipping_id, :prefectuer_id, :day_id ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :text, :price, :name, :category_id, :status_id, :shipping_id, :prefecture_id, :day_id, :sold).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :status
+  belongs_to_active_hash :day
+  belongs_to_active_hash :category
+  belongs_to :user # optional: true
+  has_one_attached :image
   with_options presence: true do
-   validates :text
-   validates :user
-   validates :price, inclusion: {in: 300..9999999 }, format: { with: /^[0-9]+$/ }
-   validates :name
-   validates :category_id
-   validates :status_id
-   validates :shipping_id
-   validates :prefectuer_id
-   validates :day_id
+    validates :text
+    validates :user
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :name
+    validates :category_id
+    validates :status_id
+    validates :shipping_id
+    validates :prefecture_id
+    validates :day_id
   end
+  validates :category_id, :status_id, :shipping_id, :prefecture_id, :day_id, numericality: { other_than: 1 }
 end
