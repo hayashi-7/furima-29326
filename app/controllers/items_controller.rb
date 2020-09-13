6d5075e5e0fa3f54a44750d43a6e432a38355ebc
hayashi-7 <tomos19088@gmail.com>
2020-09-13 17:55:02 +0900

    商品出品機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: %i[edit show]
 
   def index
-    @items = Item.order('created_at DESC')
+    #@items = Item.order('created_at DESC')
   end
 
   def new
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,10 +28,22 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Category can't be blank")
   end
 
+  it 'category_idが1だと登録できない' do
+    @item.category_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category must be other than 1")
+  end
+
   it 'statusが空だと登録できない' do
     @item.status_id = ''
     @item.valid?
     expect(@item.errors.full_messages).to include("Status can't be blank")
+  end
+
+  it 'status_idが1だと登録できない' do
+    @item.status_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Status must be other than 1")
   end
 
   it 'prefectureが空だと登録できない' do
@@ -40,15 +52,51 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Prefecture can't be blank")
   end
 
+  it 'prefecture_idが1だと登録できない' do
+    @item.prefecture_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+  end
+
   it 'shippingが空だと登録できない' do
     @item.shipping_id = ''
     @item.valid?
     expect(@item.errors.full_messages).to include("Shipping can't be blank")
   end
 
+  it 'shipping_idが1だと登録できない' do
+    @item.shipping_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Shipping must be other than 1")
+  end
+
   it 'dayが空だと登録できない' do
     @item.day_id = ''
     @item.valid?
     expect(@item.errors.full_messages).to include("Day can't be blank")
+  end
+
+  it 'day_idが1だと登録できない' do
+    @item.day_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Day must be other than 1")
+  end
+  
+  it 'priceが空だと登録できない' do
+    @item.price = ''
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it 'priceが¥300以下だと登録できない' do
+    @item.price = 299
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+  end
+
+  it 'priceが¥9999999以上だと登録できない' do
+    @item.price = 10000000
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
   end
 end
