aa1137a8c92c3e82709f367ce29f9d949674f1b7
hayashi-7 <tomos19088@gmail.com>
2020-09-17 16:29:20 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,6 +33,8 @@ class OrdersController < ApplicationController
     )
   end
 
+
+  
   def order_out
     @item = Item.find(params[:item_id])
     if user_signed_in? && current_user.id == @item.user_id
